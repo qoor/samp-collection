cdb20846b8b11a970b70b90caceae51d353dfe35
Qoo <akck1202@hanmail.net>
2019-11-21 17:40:26 +0900

    Removed unusing code

--- src/value.rs
+++ src/value.rs
@@ -24,31 +24,6 @@ pub enum PawnValue {
 	Array(PawnArray)
 }
 
-impl PawnValue {
-	pub fn get_int_value(&self) -> Result<i32, ()> {
-		match self {
-			PawnValue::Integer(value) => Ok(*value),
-			_ => Err(())
-		}
-	}
-	pub fn get_float_value(&self) -> Result<f32, ()> {
-		match self {
-			PawnValue::Float(value) => Ok(*value),
-			_ => Err(())
-		}
-	}
-	pub fn get_array_value(&self, length: i32) -> Result<&[i32], ()> {
-		match &self {
-			PawnValue::Array(value) => {
-				let array: &PawnArray = value;
-
-				Ok(&array.0[0..(length as usize)])
-			},
-			_ => Err(())
-		}
-	}
-}
-
 impl Clone for PawnValue {
 	fn clone(&self) -> PawnValue {
 		match self {
